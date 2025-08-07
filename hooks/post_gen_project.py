43cdb61c8728d54d6d9178e4e51d43c52ea64c7d
GiuseppeMinardi <38011429+GiuseppeMinardi@users.noreply.github.com>
2025-08-07 20:53:52 +0200

    Enhance post-generation script: improve Git repository setup and customize VSCode titlebar; update package installation process and fix docstring formatting rule in ruff.toml

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -7,11 +7,15 @@ from pathlib import Path
 from random import choice
 from typing import Optional
 
+# Define the root folder as the current working directory
 root_folder: Path = Path.cwd()
+
+# Regex pattern for validating GitHub URLs
 github_url_pattern: re.Pattern[str] = re.compile(
     pattern=r"^(https?://)?(www\.)?github\.com/.*$"
 )
 
+# Lists of packages to install
 data_science_packages = [
     "pymc",
     "arviz",
@@ -24,7 +28,6 @@ data_science_packages = [
     "seaborn",
     "setuptools",
     "statsmodels",
-    "scipy",
 ]
 
 dev_packages = [
@@ -34,26 +37,27 @@ dev_packages = [
 
 
 def customize_titlebar_color(color_to_use: Optional[str] = None) -> None:
-    """Customize the titlebar of vscode."""
+    """Customize the titlebar of VSCode."""
     possible_colors = ["#001524", "#15616d", "#ffecd1", "#ff7d00", "#78290f"]
     if not color_to_use:
         color_to_use = choice(possible_colors)
+
     vscode_folder: Path = root_folder.joinpath(".vscode")
     vscode_folder.mkdir(exist_ok=True, parents=True)
 
     vscode_workspace_settings = vscode_folder.joinpath("settings.json")
-
     settings = {
         "workbench.colorCustomizations": {
             "titleBar.inactiveBackground": color_to_use,
             "titleBar.inactiveForeground": "#DDDDDD",
         }
     }
+
     with vscode_workspace_settings.open("w") as f:
         json.dump(settings, f, indent=4)
 
 
-def install_packages(packages: list[str], dev: bool = ""):
+def install_packages(packages: list[str], dev: bool = False):
     """Install packages using uv."""
     try:
         commands = ["uv", "add"]
@@ -70,47 +74,67 @@ def install_packages(packages: list[str], dev: bool = ""):
         print(f"Error installing packages: {e}", file=sys.stderr)
         sys.exit(1)
 
-def setup_git_repository(git_url: str):
-    """Git Repo setup."""
-    # Regular expression to check if the URL is a GitHub URL
 
+def setup_git_repository(git_url: str):
+    """Set up a Git repository and push to the remote."""
     if not github_url_pattern.match(git_url):
         print("Error: The provided URL is not a valid GitHub URL.")
-        exit()
-
-    # Get the current directory where the project was generated
+        sys.exit(1)
 
     try:
-        # Initialize Git repository
         subprocess.run(["git", "init"], cwd=root_folder.as_posix(), check=True)
-
-        # Add remote origin using the provided Git URL
         subprocess.run(
             ["git", "remote", "add", "origin", git_url],
             cwd=root_folder.as_posix(),
             check=True,
         )
-
         print("Git repository initialized and remote origin set.")
+
+        subprocess.run(
+            ["git", "checkout", "-b", "develop"],
+            cwd=root_folder.as_posix(),
+            check=True,
+        )
+        print("Develop branch created.")
+
+        subprocess.run(
+            ["git", "add", "."],
+            cwd=root_folder.as_posix(),
+            check=True,
+        )
+        subprocess.run(
+            ["git", "commit", "-m", "first commit"],
+            cwd=root_folder.as_posix(),
+            check=True,
+        )
+        subprocess.run(
+            ["git", "push", "-u", "origin", "develop"],
+            cwd=root_folder.as_posix(),
+            check=True,
+        )
+        print("Pushed to remote develop branch.")
     except subprocess.CalledProcessError as e:
-        print(f"An error occurred: {e}")
+        print(f"An error occurred during Git operations: {e}")
+        sys.exit(1)
 
 
 def main():
-    """Install data science packages using uv."""
+    """Main function to execute post-generation tasks."""
     git_repo_url = "{{cookiecutter.repo_git_ssh_url}}"
     if git_repo_url:
-        print("Initializing git repo.")
+        print("Initializing Git repo.")
         setup_git_repository(git_repo_url)
     else:
-        print("No github repo provided.")
-    print("Installing oackages...")
+        print("No GitHub repo provided.")
+
+    print("Installing packages...")
     install_packages(data_science_packages)
-    print("Installing packages for dev...")
+
+    print("Installing dev packages...")
     install_packages(dev_packages, dev=True)
+
     print("Customizing VSCode...")
     customize_titlebar_color()
-
 
 if __name__ == "__main__":
     main()
