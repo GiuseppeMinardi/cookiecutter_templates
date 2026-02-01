b7e8144a11947ddb84c6f0ea50a453c021cf6ce2
GiuseppeMinardi <38011429+GiuseppeMinardi@users.noreply.github.com>
2026-02-01 18:48:16 +0100

    Refactor notebook imports and add main application file with initial setup

---
+++ {{ cookiecutter.repo_name }}/main.py
@@ -0,0 +1,8 @@
+"""Docstring for {{ cookiecutter.repo_name }} main file."""
+
+def main():
+    """Main Function."""  # noqa: D401
+    print("Starting {{ cookiecutter.repo_name }} application...")
+
+if __name__ == "__main__":
+    main()
